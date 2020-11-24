19f7110f42ecc980fe368af03c59d383f3ab16fd
bchadwic <73488828+bchadwic@users.noreply.github.com>
2020-11-24 12:51:59 -0800

    Create SnakeClient.java

---
+++ src/client/SnakeClient.java
@@ -0,0 +1,11 @@
+package client;
+
+import game.SnakeGame;
+
+public class SnakeClient {
+
+    public static void main(String[] args) {
+        SnakeGame newGame = new SnakeGame();
+        newGame.start();
+    }
+}
